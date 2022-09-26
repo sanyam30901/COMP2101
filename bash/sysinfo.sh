0ef806b1653f29f1a4602124324dedef121c6a77
Sanyam Gupta <sanyam@pc200496906>
2022-09-26 14:03:56 -0400

    Lab 2

---
+++ bash/sysinfo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+hostnamectl
+#showing details about the system information,hostname,operating system, kernel version and machine id.
+hostname -I
+#show the ip address of the machine
+df -h /
+#amount of available disk space in a human friendly number
